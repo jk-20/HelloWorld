cc9eb61b13b9443bd1bab3481e4939605567f0d5
jogindra_kumar <joginder9868@outlook.com>
2020-04-26 08:40:24 +0530

    new file:   .idea/vcs.xml 	modified:   out/production/HelloWorld/com/company/Main.class 	modified:   src/com/company/Main.java

--- HelloWorld/src/com/company/Main.java
+++ HelloWorld/src/com/company/Main.java
@@ -6,8 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        Date now = new Date();
-        System.out.println(now);
+        System.out.println("Hello World");
 
     }
 }
